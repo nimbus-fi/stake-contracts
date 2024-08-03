12fe3374d26bd14c53acdfb8930ffa130dad3c10
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-08-03 05:31:46 +0530

    feat: create stEDU token contract

---
+++ contracts/stEDU.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// StETH Token Contract
+contract stEDU is ERC20, Ownable {
+    constructor() ERC20("Staked Ether", "stETH") {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
